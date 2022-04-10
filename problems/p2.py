f230b6da0712c18dbd686da0ed3d38e3bbb82342
Francis Luong (Franco) <franco@definefunk.com>
2022-04-10 10:09:52 -0400

    addTwoNumbers: Accepted

--- problems/p2.py
+++ problems/p2.py
@@ -1,5 +1,6 @@
+import math
 from leet.linklist import ListNode
-from typing import List
+from typing import List, Optional
 
 """
 https://leetcode.com/problems/add-two-numbers/
@@ -15,10 +16,45 @@ Explanation: 342 + 465 = 807.
 """
 
 
+def val(node) -> int:
+    return node.val if node else 0
+
+
+def sum_mod_10(v1: int, v2: int, carryover: Optional[int] = 0) -> (int, int):
+    sum = v1 + v2 + carryover
+    value = sum % 10
+    new_carry = math.floor(sum / 10)
+    return new_carry, value
+
+
 class Solution:
     def brute_naive(
         self,
         l1: ListNode,
         l2: ListNode,
     ) -> ListNode:
-        pass
+        # lists may not be same length
+        # and we have to factor for carryover
+        result_node = None
+        prev_node = None
+        carryover = 0
+        while l1 or l2 or carryover:
+            # get new sum and carryover, and create a new node
+            carryover, sum = sum_mod_10(val(l1), val(l2), carryover=carryover)
+            new_node = ListNode(sum)
+            # link to previous node if we can
+            if prev_node:
+                prev_node.next = new_node
+            # prep next iteration
+            result_node = result_node or new_node
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            prev_node = new_node
+        # handle case where l1 and l2 were None
+        result_node = result_node or ListNode()
+        return result_node
+
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        return self.brute_naive(l1, l2)
--- tests/problems/test_p2.py
+++ tests/problems/test_p2.py
@@ -23,8 +23,21 @@ class TestP2(unittest.TestCase):
             case = str(
                 ["Case:", solver_fn.__name__, listnode1.values(), listnode2.values()]
             )
-            result = solver_fn(listnode1, listnode2)
+            result = solver_fn(listnode1, listnode2).values()
             self.assertEqual(expected, result, case)
+
+    def test_sum_mod_10(self):
+        v1_v2_expected = [
+            [1, 2, 0, (0, 3)],
+            [1, 2, 7, (1, 0)],
+            [11, 2, 0, (1, 3)],
+        ]
+        for case_params in v1_v2_expected:
+            case = str(case_params)
+            v1, v2, carryover, expected = case_params
+            self.assertEqual(
+                expected, sut.sum_mod_10(v1, v2, carryover=carryover), case
+            )
 
     def test_brute_naive(self):
         return self.run_cases(self.sut.brute_naive)
